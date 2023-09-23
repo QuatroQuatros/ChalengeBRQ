7a20e47f72677ac6e8ccfd2255640d67f97677a4
dandara <dandaracarvalhoestudos@gmail.com>
2023-09-22 23:16:42 -0300

    featTelaVisualizarCarro

---
+++ assets/js/script_VisualizaCarro.js
@@ -0,0 +1,36 @@
+const slider = document.querySelectorAll('.slider')
+const btnPrev = document.getElementById("prev_button")
+const btnNext = document.getElementById("next_button")
+
+let currentSLide = 0;
+
+function hideSlider() {
+    slider.forEach(item => item.classList.remove('on'))
+}
+
+function showSlider() {
+    slider[currentSLide].classList.add('on')
+}
+
+function nextSlider() {
+    hideSlider()
+    if(currentSLide === slider.length - 1) {
+        currentSLide = 0
+    }else {
+        currentSLide++
+    }
+
+    showSlider()
+}
+
+function prevSlider() {
+    hideSlider()
+    if(currentSLide === 0) {
+        currentSLide = slider.length - 1
+    }else {
+        currentSLide--
+    }
+}
+
+btnNext.addEventListener('click', nextSlider)
+btnPrev.addEventListener('click', prevSlider)
