8a9398cca4f448500b5b8e18c4d9a814e709ef77
Gabriela Camargo Oliveira <gabriela.camaroli@gmail.com>
2023-10-10 22:55:07 -0300

    Populando tabelas e consulta por despesas e investimentos

--- SQL/InsertsUpdates/updateUser.sql
+++ SQL/InsertsUpdates/updateUser.sql
@@ -1,7 +1,6 @@
-UPDATE users SET name = '4444' WHERE id = 1
-
 UPDATE despesas
 SET valor = 50.75, 
     descricao = 'Despesa Atualizada', 
-    data_vencimento = '2023-12-20'
+    data_vencimento = '2023-12-20',
+    updated_at = NOW()
 WHERE id = 1;
