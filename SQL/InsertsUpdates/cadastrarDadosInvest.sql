84f7344f6ea3edcbc8f0103ef0497df3d3f86933
Yussef-Assad <129677389+Yussef-Assad@users.noreply.github.com>
2023-10-13 20:42:37 -0300

    DadosUserInvest

---
+++ SQL/InsertsUpdates/cadastrarDadosInvest.sql
@@ -0,0 +1,6 @@
+INSERT INTO investimentos (id_usuario, id_corretora, ativo, valor, juros, data_compra, data_vencimento)
+VALUES ();
+
+UPDATE investimentos
+SET ativo = , valor = , juros = , data_compra = , data_vencimento = 
+WHERE id = ; 
